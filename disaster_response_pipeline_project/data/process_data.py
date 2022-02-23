88e56f9eafe397103ccf6be58f4d078976c942bc
keithwu <keith.wu@outlook.com>
2022-02-23 15:35:45 -0800

    complete ETL pipeline
    complete the process_data.py

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -1,19 +1,59 @@
 import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    ''' load two datasets and merge them into one dataframe
+        return the merged dataframe
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = pd.merge(messages, categories, how='outer', on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    ''' clean the given dataframe
+        return the cleaned dataframe
+    '''
+
+    # Split categories into separate category columns
+    # use this row to extract a list of new column names for categories.
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x.split('-')[0])
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1.
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x: x.split('-')[-1] )
+        categories[column] = categories[column].apply(lambda x: int(x))
+
+
+    # Replace categories column in df with new category columns.
+    df.drop(columns='categories', inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    ''' Save the clean dataset into an sqlite database.
+    '''
+    engine = create_engine('sqlite:///InsertDatabaseName.db')
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
+    ''' ETL pipeline process
+    '''
+    
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
